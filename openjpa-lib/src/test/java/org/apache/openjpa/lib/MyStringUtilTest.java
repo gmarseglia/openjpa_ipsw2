85d672817dda072e26c58920f62b861a7ae81b7b
gmarseglia <g.marseglia.it@gmail.com>
2025-01-27 11:44:32 +0100

    Updated pom, jacoco profile working

---
+++ openjpa-lib/src/test/java/org/apache/openjpa/lib/MyStringUtilTest.java
@@ -0,0 +1,14 @@
+package org.apache.openjpa.lib;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class MyStringUtilTest {
+
+    @Test
+    public void prova() {
+        Assertions.assertEquals(1, 1);
+    }
+
+}
